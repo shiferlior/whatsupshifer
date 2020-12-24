5b297be4803b1148d91b737ccb93433fedbecd65
shiferlior <shiferlior@gmail.com>
2020-12-25 01:37:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const accountSid = 'AC54a4e2215c672f4b3527bb11b5a65ed3'; 
-const authToken = '${{ secrets.token_twilio }}'; 
+const authToken = '837ff4ca020c02a2804d3d9ac823cba0'; 
 const client = require('twilio')(accountSid, authToken); 
  
 client.messages 
