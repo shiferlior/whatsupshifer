9b3b3af2501a8d3da569a84c84d82c467601c1e9
Lior Shifer <shiferlior@gmail.com>
2020-12-25 12:11:39 +0200

    remove send

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const accountSid = 'AC54a4e2215c672f4b3527bb11b5a65ed3';
 const authToken = 'cc6761ad01d9eaf4f392139d341463ad';
 const client = require('twilio')(accountSid, authToken);
 
+whatsapp://send?phone=+14155238886&text=join+condition-neighbor
+
 //Send message
-client.messages
-    .create({
-        body: 'Amazing!',
-        from: 'whatsapp:+14155238886',
-        to: 'whatsapp:+972526010065'
-    })
-    .then(message => console.log(message.sid))
-    .done();
+// client.messages
+//     .create({
+//         body: 'Amazing!',
+//         from: 'whatsapp:+14155238886',
+//         to: 'whatsapp:+972526010065'
+//     })
+//     .then(message => console.log(message.sid))
+//     .done();
 
 //Server conversation
 const http = require('http');
