b42da0aede3bc0e0c464554a4d3505b9c29675e8
Lior Shifer <shiferlior@gmail.com>
2020-12-25 13:03:40 +0200

    fix

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const accountSid = 'AC54a4e2215c672f4b3527bb11b5a65ed3';
 const authToken = 'cc6761ad01d9eaf4f392139d341463ad';
 const client = require('twilio')(accountSid, authToken);
 
-whatsapp://send?phone=+14155238886&text=join+condition-neighbor
 
 //Send message
 // client.messages
