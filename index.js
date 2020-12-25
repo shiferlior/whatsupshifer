510dca2d48b629bd4ca3b412806175d3f93374eb
Lior Shifer <shiferlior@gmail.com>
2020-12-25 02:22:53 +0200

    try conversation

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
-const accountSid = 'AC54a4e2215c672f4b3527bb11b5a65ed3'; 
-const authToken = 'cc6761ad01d9eaf4f392139d341463ad'; 
-const client = require('twilio')(accountSid, authToken); 
- 
+const accountSid = 'AC54a4e2215c672f4b3527bb11b5a65ed3';
+const authToken = 'cc6761ad01d9eaf4f392139d341463ad';
+const client = require('twilio')(accountSid, authToken);
+
 //Send message
-client.messages 
-      .create({ 
-         body: 'Amazing!', 
-         from: 'whatsapp:+14155238886',       
-         to: 'whatsapp:+972526010065' 
-       }) 
-      .then(message => console.log(message.sid)) 
-      .done();
+client.messages
+    .create({
+        body: 'Amazing!',
+        from: 'whatsapp:+14155238886',
+        to: 'whatsapp:+972526010065'
+    })
+    .then(message => console.log(message.sid))
+    .done();
+
+//Server conversation
+const http = require('http');
+const express = require('express');
+const session = require('express-session');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const app = express();
+
+app.use(session({ secret: 'anything-you-want-but-keep-secret' }));
+
+app.post('/messages', (req, res) => {
+    const smsCount = req.session.counter || 0;
+
+    let message = 'Hello, thanks for the new message.';
+
+    if (smsCount > 0) {
+        message = 'Hello, thanks for message number ' + (smsCount + 1);
+    }
+
+    req.session.counter = smsCount + 1;
+
+    const twiml = new MessagingResponse();
+    twiml.message(message);
+
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+});
+
+http.createServer(app).listen(process.env.PORT || 3000, () => {
+    console.log('Express server listening on port 3000');
+});
